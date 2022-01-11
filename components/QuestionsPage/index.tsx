d2365f00501325e4c93bcc36e8fecc7ebe42245b
Adriano Castro <adrianosantosdoco@gmail.com>
2022-01-11 02:55:11 -0300

    questions page done, miss the functions on the options buttons

--- components/QuestionsPage/index.tsx
+++ components/QuestionsPage/index.tsx
@@ -3,19 +3,19 @@ import styles from './Index.module.css'
 
 
 const itemsProfilingTest = ProfilingTest.questions.map((questions) =>
-    <div className={styles.holderQuestions}>
-        <div className={styles.holderNumberAndAsk}>
-            <h3 className={styles.number}>{questions.number}.</h3>
-            <h3 className={styles.ask}>{questions.ask}</h3>
+    <div className={styles.cardQuestions}>
+        <div className={styles.holderNumberAndAsk} key={questions.key}>
+            <h4 className={styles.number}>{questions.number}.</h4>
+            <h4 className={styles.ask}>{questions.ask}</h4>
         </div>
         <div className={styles.holderOptions}>
             <div className={styles.holderOptionA}>
-                <button className={styles.buttonOption} />
+                <input type={'checkbox'} className={styles.buttonOption} />
                 <h4 className={styles.optionA}>{questions.optionA}
                 </h4>
             </div>
             <div className={styles.holderOptionB}>
-                <button className={styles.buttonOption} />
+                <input type={'checkbox'} className={styles.buttonOption} />
                 <h4 className={styles.optionB}>{questions.optionB}</h4>
             </div>
         </div>
